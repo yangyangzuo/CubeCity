5dd77f0d9d73c227d32118c8324d9807f1595909
zuoyang <15138995938@163.com>
2026-04-19 15:57:30 +0800

    移除无关内容

--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-import antfu from '@antfu/eslint-config'
-
-export default antfu({
-  ignores: ['node_modules', 'node_modules/**', '.pnp', '.pnp/**', '.pnp.js', '**/.pnp.js/**', 'coverage', 'coverage/**', 'test-results/', 'test-results/**/', 'playwright-report/', 'playwright-report/**/', 'playwright/.cache/', 'playwright/.cache/**/', 'dist', 'dist/**', 'high-level-dependencies.html', '**/high-level-dependencies.html/**', '.DS_Store', '**/.DS_Store/**', '*.pem', '**/*.pem/**', 'npm-debug.log*', '**/npm-debug.log*/**', 'yarn-debug.log*', '**/yarn-debug.log*/**', 'yarn-error.log*', '**/yarn-error.log*/**', '.pnpm-debug.log*', '**/.pnpm-debug.log*/**', '.env*.local', '**/.env*.local/**', '.vercel', '**/.vercel/**'],
-})
